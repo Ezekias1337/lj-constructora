d2e40f302f2cc4359aa228078807144cedf95813
Frank Edwards <frankedwards50@gmail.com>
2026-08-18 00:51:26 -0700

    Updates

--- assets/GeneralLink-qHUSmmpx.js
+++
@@ -1,2 +0,0 @@
-import{j as s,L as c}from"./index-w12HYpc0.js";const o=({title:r,includeBanner:n,additionalClassNames:e})=>s.jsx("h1",{className:`page-title ${e!==void 0?e:""} ${n===!0?"banner-title":""}`,children:r}),p=({text:r,url:n,openInNewTab:e,additionalClassNames:a,variant:i="primary"})=>s.jsx(c,{to:n,params:t=>t,search:t=>t,className:`general-link ${i}-link ${a!==void 0?a:""}`,target:e?"_blank":"",children:r});export{p as G,o as P};
-//# sourceMappingURL=GeneralLink-qHUSmmpx.js.map
